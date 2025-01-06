f265cb566c9513ffe31a4ac2e1a9160b6c4ecc01
greg <luca.demelo18@gmail.com>
2025-01-06 16:14:20 +0100

    V2

--- src/main/java/fr/eseo/Modele/Calculatrice.java
+++ src/main/java/fr/eseo/Modele/Calculatrice.java
@@ -1,4 +1,43 @@
 package fr.eseo.Modele;
 
 public class Calculatrice {
+    int chiffre1;
+    int chiffre2;
+
+    public Calculatrice(int chiffre1, int chiffre2) {
+        this.chiffre1 = chiffre1;
+        this.chiffre2 = chiffre2;
+    }
+
+    //Fonction qui multiplie le premier chiffre avec le deuxième.
+    public int multi() {
+        return chiffre1 * chiffre2;
+    }
+
+    //Fonction qui additionne le premier chiffre avec le deuxième.
+    public int plus() {
+        return chiffre1 + chiffre2;
+    }
+
+    //Fonction qui soustrait le premier chiffre au deuxième.
+    public int minus() {
+        return chiffre1 - chiffre2;
+    }
+
+    //Getter et Setter des chiffres.
+    public int getChiffre1() {
+        return chiffre1;
+    }
+
+    public void setChiffre1(int chiffre1) {
+        this.chiffre1 = chiffre1;
+    }
+
+    public int getChiffre2() {
+        return chiffre2;
+    }
+
+    public void setChiffre2(int chiffre2) {
+        this.chiffre2 = chiffre2;
+    }
 }
