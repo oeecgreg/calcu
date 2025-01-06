71dcb7b3b21aaa7cb6ca43f8662f0849f12cc25b
Alexander <alexanderbelhareth@gmail.com>
2025-01-06 17:01:26 +0100

    V3

--- src/main/java/fr/eseo/Main.java
+++ src/main/java/fr/eseo/Main.java
@@ -1,14 +1,26 @@
 package fr.eseo;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to calculatrice :) \n \n" +
                 "Quelle opération souhaitez-vous faire ? :\n \n" +
-                "1- Addition (+).\n" +
-                "2- Soustraction (-).\n" +
-                "3- Multiplication (*).\n" +
-                "4- Quitter.\n");
+                "1- Addition (+)\n" +
+                "2- Soustraction (-)\n" +
+                "3- Multiplication (*)\n" +
+                "4- Quitter");
+        int operation = scanner.nextInt();
+        System.out.println("you chose " + operation);
+        System.out.println("Choisi ton premier nombre");
+        int nombre1 = scanner.nextInt();
+        System.out.println("you chose " + nombre1);
+        System.out.println("Choisi ton deuxieme nombre");
+        int nombre2 = scanner.nextInt();
+        System.out.println("you chose " + nombre2);
+
+
     }
 }
